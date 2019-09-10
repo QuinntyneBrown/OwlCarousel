94499e3af62a7d4551e63179983523814fee9b43
Quinntyne Brown <Quinntyne.Brown@trader.ca>
2019-09-09 22:25:11 -0400

    update

--- Program.cs
+++ Program.cs
@@ -1,5 +1,9 @@
 using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 namespace OwlCarousel
@@ -12,5 +16,33 @@ namespace OwlCarousel
         public static IWebHostBuilder CreateHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder()
                 .UseStartup<Startup>();
+    }
+
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+            => Configuration = configuration;
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+            => services.AddMvc();
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute("default", "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+            => View();
     }
 }
